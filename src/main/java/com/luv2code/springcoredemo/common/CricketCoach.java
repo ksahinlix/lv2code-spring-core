dd872b72eef2fec9672412e02406c3fd1ca5e0cb
Kaan Berk Şahinli <kaanberksahinli@eskisehir.edu.tr>
2023-06-04 15:45:25 +0300

    made some examples for component scanning

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,9 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,7 +1,7 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
 
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
